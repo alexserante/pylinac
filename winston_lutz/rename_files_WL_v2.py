d0ebdd5b24901b4754b3f882de737343bf1ff59d
alexserante <alexandre.serante@gmail.com>
2022-11-23 18:03:09 -0300

    Update rename_files_WL_v2.py

--- winston_lutz/rename_files_WL_v2.py
+++ winston_lutz/rename_files_WL_v2.py
@@ -14,7 +14,6 @@ def handle_click(event):
         print("Nenhuma pasta selecionada!")
         return
 
-
     # positions of gantry, col and couch
     gantry = [0, 90, 180, 270, 0, 0, 0, 0]
     colimator = [0, 0, 0, 0, 90, 270, 0, 0]
@@ -66,18 +65,20 @@ window = tk.Tk()
 
 
 # Frame to select folder where the dcm images are
-frm_select_folder = tk.LabelFrame(master=window, text="Select folder", font="VERDANA")
+frm_select_folder = tk.LabelFrame(
+    master=window, text="Select folder", font="VERDANA")
 frm_select_folder.grid(row=0, column=0)
 
 lbl_select_folder = tk.Label(
     master=frm_select_folder, text="Clique no botão para selecionar a pasta", font="VERDANA")
 lbl_select_folder.grid(row=0, column=0)
 
-button = tk.Button(master=frm_select_folder, text="Selecionar pasta", font="VERDANA")
+button = tk.Button(master=frm_select_folder,
+                   text="Selecionar pasta", font="VERDANA")
 button.bind("<Button-1>", handle_click)
 button.grid(row=1, column=0)
 
-'''# Frame to choose from which LINAC the WL was run
+# Frame to choose from which LINAC the WL was run
 frm_select_linac = tk.Frame(master=window, borderwidth=1, relief="raised")
 frm_select_linac.grid(row=0, column=1)
 
@@ -90,10 +91,11 @@ rbtn_linac_6 = tk.Radiobutton(
     master=frm_select_linac, text="AL6", variable=var_linac, value="AL6")
 rbtn_linac_4.grid(row=0, column=0)
 rbtn_linac_5.grid(row=1, column=0)
-rbtn_linac_6.grid(row=2, column=0)'''
+rbtn_linac_6.grid(row=2, column=0)
 
-frm_perform_analysis = tk.LabelFrame(master=window, text="WL analysis", font="VERDANA")
-frm_perform_analysis.grid(row=0, column=1)
+frm_perform_analysis = tk.LabelFrame(
+    master=window, text="WL analysis", font="VERDANA")
+frm_perform_analysis.grid(row=0, column=2)
 
 btn_perform_analysis = tk.Button(
     master=frm_perform_analysis, text="Fazer análise WL", font="VERDANA")
