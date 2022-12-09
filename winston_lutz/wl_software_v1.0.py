e466c4344c21ce33991b13ad738545c3f403e3e8
alexserante <alexandre.serante@gmail.com>
2022-12-09 15:45:46 -0300

    Update wl_software_v1.0.py

--- winston_lutz/wl_software_v1.0.py
+++ winston_lutz/wl_software_v1.0.py
@@ -108,8 +108,14 @@ def analyze_wl():
     message_console(text_console)
 
 
-def show_images(axis):
-    wl.plot_images(axis=axis)
+def show_images_gantry():
+    wl.plot_images(axis="Gantry")
+
+def show_images_col():
+    wl.plot_images(axis="Collimator")
+
+def show_images_couch():
+    wl.plot_images(axis="Couch")
 
 
 def show_plots():
@@ -130,48 +136,79 @@ def save_pdf():
 window = tk.Tk()
 
 frm_left = tk.Frame(master=window)
-frm_left.grid(row=0, column=0)
+frm_left.grid(row=0, column=0, sticky="n")
 
 # Frame to select folder where the dcm images are
 frm_select_folder = tk.LabelFrame(
     master=frm_left, text="Select folder", font="VERDANA")
-frm_select_folder.grid(row=0, column=0)
+frm_select_folder.grid(row=0, column=0, padx=10, pady=5)
 
-button = tk.Button(master=frm_select_folder,
+btn_select_folder = tk.Button(master=frm_select_folder,
                    text="Selecionar pasta", font="VERDANA",
-                   command=open_files_path).grid(row=0, column=0)
+                   command=open_files_path).grid(row=0, column=0, padx=10, pady=5)
 
-button = tk.Button(master=frm_select_folder,
+btn_format_images = tk.Button(master=frm_select_folder,
                    text="Formatar imagens", font="VERDANA",
-                   command=format_images).grid(row=1, column=0, padx=10, pady=20)
+                   command=format_images).grid(row=1, column=0, padx=10, pady=5)
 
 
 frm_perform_analysis = tk.LabelFrame(
     master=frm_left, text="WL analysis", font="VERDANA")
-frm_perform_analysis.grid(row=0, column=2)
+frm_perform_analysis.grid(row=0, column=1, padx=10, pady=5)
 
 btn_perform_analysis = tk.Button(
     master=frm_perform_analysis, text="Fazer análise WL", font="VERDANA",
-    command=analyze_wl).grid(row=0, column=0)
+    command=analyze_wl).grid(row=0, column=0, padx=10, pady=5)
 
 btn_plot_summary = tk.Button(
     master=frm_perform_analysis, text="Plotar gráficos", font="VERDANA",
-    command=show_plots).grid(row=2, column=0)
+    command=show_plots).grid(row=2, column=0, padx=10, pady=5)
 
 
 frm_images = tk.LabelFrame(master=frm_left, text="Imagens", font="VERDANA")
-frm_images.grid(row=1, column=0, columnspan=2)
+frm_images.grid(row=1, column=0, columnspan=2, padx=20, pady=5)
 
 
 btn_gantry_images = tk.Button(
     master=frm_images, text="Gantry", font="VERDANA",
-    command=show_images("Gantry")).grid(row=0, column=0)
+    command=show_images_gantry).grid(row=0, column=0, padx=10, pady=5)
 btn_collimator_images = tk.Button(
     master=frm_images, text="Colimador", font="VERDANA",
-    command=show_images("Collimator")).grid(row=0, column=1)
+    command=show_images_col).grid(row=0, column=1, padx=10, pady=5)
 btn_couch_images = tk.Button(
     master=frm_images, text="Mesa", font="VERDANA",
-    command=show_images("Couch")).grid(row=0, column=2)
+    command=show_images_couch).grid(row=0, column=2, padx=10, pady=5)
+
+frm_save_pdf = tk.LabelFrame(master=frm_left, text="Salvar resultados", font="VERDANA")
+frm_save_pdf.grid(row=2, column=0, columnspan=2, padx=20, pady=5, sticky="w")
+
+# Frame to choose from which LINAC the WL was run
+frm_select_linac = tk.Frame(master=frm_save_pdf, borderwidth=1, relief="raised")
+frm_select_linac.grid(row=0, column=0)
+
+var_linac = ""
+rbtn_linac_1 = tk.Radiobutton(
+    master=frm_select_linac, text="AL1", variable=var_linac, value="AL1")
+rbtn_linac_2 = tk.Radiobutton(
+    master=frm_select_linac, text="AL2", variable=var_linac, value="AL2")
+rbtn_linac_3 = tk.Radiobutton(
+    master=frm_select_linac, text="AL3", variable=var_linac, value="AL3")
+rbtn_linac_4 = tk.Radiobutton(
+    master=frm_select_linac, text="AL4", variable=var_linac, value="AL4")
+rbtn_linac_5 = tk.Radiobutton(
+    master=frm_select_linac, text="AL5", variable=var_linac, value="AL5")
+rbtn_linac_6 = tk.Radiobutton(
+    master=frm_select_linac, text="AL6", variable=var_linac, value="AL6")
+rbtn_linac_1.grid(row=0, column=0)
+rbtn_linac_2.grid(row=1, column=0)
+rbtn_linac_3.grid(row=2, column=0)
+rbtn_linac_4.grid(row=0, column=1)
+rbtn_linac_5.grid(row=1, column=1)
+rbtn_linac_6.grid(row=2, column=1)
+
+btn_save_pdf = tk.Button(
+    master=frm_save_pdf, text="Salvar PDF", font="VERDANA",
+    command=save_pdf).grid(row=0, column=2, padx=10, pady=5)
 
 
 frm_results = tk.LabelFrame(
@@ -196,20 +233,5 @@ frm_console = tk.LabelFrame(
     master=window, width=800, height=50, text="Console")
 frm_console.grid(row=2, column=0, columnspan=4, sticky="nw")
 
-
-# Frame to choose from which LINAC the WL was run
-'''frm_select_linac = tk.Frame(master=window, borderwidth=1, relief="raised")
-frm_select_linac.grid(row=0, column=1)
-
-var_linac = ""
-rbtn_linac_4 = tk.Radiobutton(
-    master=frm_select_linac, text="AL4", variable=var_linac, value="AL4")
-rbtn_linac_5 = tk.Radiobutton(
-    master=frm_select_linac, text="AL5", variable=var_linac, value="AL5")
-rbtn_linac_6 = tk.Radiobutton(
-    master=frm_select_linac, text="AL6", variable=var_linac, value="AL6")
-rbtn_linac_4.grid(row=0, column=0)
-rbtn_linac_5.grid(row=1, column=0)s
-rbtn_linac_6.grid(row=2, column=0)'''
 
 window.mainloop()
