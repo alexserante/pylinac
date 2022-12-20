f9ba8860998ec098a5d1f5d4aab150d532f9c7e0
alexserante <alexandre.serante@gmail.com>
2022-12-20 13:45:59 -0300

    Update wl_software_v1.0.py

--- winston_lutz/wl_software_v1.0.py
+++ winston_lutz/wl_software_v1.0.py
@@ -105,6 +105,8 @@ def analyze_wl():
     lbl_results.config(text=wl.results())
     lbl_shift_bb.config(text="Mover: " + wl.bb_shift_instructions())
 
+    print(wl.bb_shift_vector().get())
+
     # show message in console
     text_console = "Análise concluída!"
     message_console(text_console)
@@ -129,7 +131,12 @@ def show_plots():
 def save_pdf():
     date = datetime.today().strftime("%Y%m%d")
     path = main_path + "/WL-" + date + ".pdf"
-    wl.publish_pdf(path)
+
+    dem_dict = {'Autor': '', 'Acelerador': ''}
+    dem_dict['Autor'] = name_var.get()
+    dem_dict['Acelerador'] = acelerator_var.get()
+    wl.publish_pdf(filename=path, metadata=dem_dict)
+
     # show message in console
     text_console = "PDF salvo em: " + path
     message_console(text_console)
@@ -149,20 +156,34 @@ frm_left.grid(row=0, column=0, sticky="n")
 
 # Frame to select folder where the dcm images are
 frm_select_folder = tk.LabelFrame(
-    master=frm_left, text="Select folder", font="VERDANA")
+    master=frm_left, text="Diretório", font="VERDANA")
 frm_select_folder.grid(row=0, column=0, padx=10, pady=5)
 
 btn_select_folder = tk.Button(master=frm_select_folder,
                               text="Selecionar pasta", font="VERDANA",
-                              command=open_files_path).grid(row=0, column=0, padx=10, pady=5)
+                              command=open_files_path).grid(row=0, column=0,
+                                                            columnspan=2, padx=10, pady=5)
+
+lbl_wl_type = tk.Label(master=frm_select_folder,
+                       text="Tipo WL:").grid(row=1, column=0, sticky="w")
+
+var_wl_type = StringVar()
+rbtn_wl_type_1 = tk.Radiobutton(
+    master=frm_select_folder, text="Completo",
+    variable=var_wl_type, value="Completo").grid(row=3, column=0)
+rbtn_wl_type_2 = tk.Radiobutton(
+    master=frm_select_folder, text="Básico",
+    variable=var_wl_type, value="Básico").grid(row=3, column=1)
 
 btn_format_images = tk.Button(master=frm_select_folder,
-                              text="Formatar imagens", font="VERDANA",
-                              command=format_images).grid(row=1, column=0, padx=10, pady=5)
+                              text="Formatar imagens",
+                              font="VERDANA",
+                              command=format_images).grid(row=5, column=0,
+                                                          columnspan=2, padx=10, pady=5)
 
 
 frm_perform_analysis = tk.LabelFrame(
-    master=frm_left, text="WL analysis", font="VERDANA")
+    master=frm_left, text="Análise WL", font="VERDANA")
 frm_perform_analysis.grid(row=0, column=1, padx=10, pady=5)
 
 btn_perform_analysis = tk.Button(
@@ -177,7 +198,6 @@ btn_plot_summary = tk.Button(
 frm_images = tk.LabelFrame(master=frm_left, text="Imagens", font="VERDANA")
 frm_images.grid(row=1, column=0, columnspan=2, padx=20, pady=5)
 
-
 btn_gantry_images = tk.Button(
     master=frm_images, text="Gantry", font="VERDANA",
     command=show_images_gantry).grid(row=0, column=0, padx=10, pady=5)
@@ -188,62 +208,50 @@ btn_couch_images = tk.Button(
     master=frm_images, text="Mesa", font="VERDANA",
     command=show_images_couch).grid(row=0, column=2, padx=10, pady=5)
 
-frm_save_pdf = tk.LabelFrame(
-    master=frm_left, text="Salvar resultados", font="VERDANA")
-frm_save_pdf.grid(row=2, column=0, columnspan=2, padx=20, pady=5, sticky="w")
 
-# Frame to choose from which LINAC the WL was run
-'''frm_select_linac = tk.Frame(
-    master=frm_save_pdf, borderwidth=1, relief="raised")
-frm_select_linac.grid(row=0, column=0)
-
-var_linac = StringVar()
-rbtn_linac_1 = tk.Radiobutton(
-    master=frm_select_linac, text="AL1", variable=var_linac, value="AL1")
-rbtn_linac_2 = tk.Radiobutton(
-    master=frm_select_linac, text="AL2", variable=var_linac, value="AL2")
-rbtn_linac_3 = tk.Radiobutton(
-    master=frm_select_linac, text="AL3", variable=var_linac, value="AL3")
-rbtn_linac_4 = tk.Radiobutton(
-    master=frm_select_linac, text="AL4", variable=var_linac, value="AL4")
-rbtn_linac_5 = tk.Radiobutton(
-    master=frm_select_linac, text="AL5", variable=var_linac, value="AL5")
-rbtn_linac_6 = tk.Radiobutton(
-    master=frm_select_linac, text="AL6", variable=var_linac, value="AL6")
-rbtn_linac_1.grid(row=0, column=0)
-rbtn_linac_2.grid(row=1, column=0)
-rbtn_linac_3.grid(row=2, column=0)
-rbtn_linac_4.grid(row=0, column=1)
-rbtn_linac_5.grid(row=1, column=1)
-rbtn_linac_6.grid(row=2, column=1)'''
-
-
-btn_save_pdf = tk.Button(
-    master=frm_save_pdf, text="Salvar PDF", font="VERDANA",
-    command=save_pdf).grid(row=0, column=2, padx=10, pady=5)
-
+frm_right = tk.Frame(master=window)
+frm_right.grid(row=0, column=1, sticky="n")
 
 frm_results = tk.LabelFrame(
-    master=window, width=435, height=250, text="Resultados", font="VERDANA")
-frm_results.grid(row=0, column=3)
+    master=frm_right, width=435, height=250, text="Resultados", font="VERDANA")
+frm_results.grid(row=0, column=3, sticky='n')
 frm_results.grid_propagate(0)
 
 lbl_results = tk.Label(master=frm_results)
 lbl_results.grid(row=0, column=0)
 
 frm_shift_bb = tk.LabelFrame(
-    master=window, width=435, height=50, text="Shift", font="VERDANA")
-frm_shift_bb.grid(row=1, column=3)
+    master=frm_right, width=435, height=50, text="Shift", font="VERDANA")
+frm_shift_bb.grid(row=1, column=3, sticky='n', pady=10)
 frm_shift_bb.grid_propagate(0)
 
 lbl_shift_bb = tk.Label(master=frm_shift_bb, text="", fg="Red", font="VERDANA")
 lbl_shift_bb.grid(row=0, column=0)
 
 
+frm_save_pdf = tk.LabelFrame(
+    master=frm_left, text="Salvar resultados", font="VERDANA")
+frm_save_pdf.grid(row=2, column=0, columnspan=2, padx=20, pady=5, sticky="w")
+
+name_var = StringVar()
+acelerator_var = StringVar()
+lbl_name = tk.Label(master=frm_save_pdf, text="Autor: ").grid(row=0, column=0)
+entry_name = tk.Entry(master=frm_save_pdf,
+                      textvariable=name_var).grid(row=0, column=1)
+lbl_acelerador = tk.Label(
+    master=frm_save_pdf, text="Acelerador: ").grid(row=1, column=0)
+entry_acelerator = tk.Entry(
+    master=frm_save_pdf, textvariable=acelerator_var, width=3).grid(row=1, column=1, sticky="w")
+
+btn_save_pdf = tk.Button(
+    master=frm_save_pdf, text="Salvar PDF", font="VERDANA",
+    command=save_pdf).grid(row=0, column=2, rowspan=2, padx=10, pady=10)
+
+
 # Console frame
 frm_console = tk.LabelFrame(
     master=window, width=800, height=50, text="Console")
-frm_console.grid(row=2, column=0, columnspan=4, sticky="nw")
+frm_console.grid(row=2, column=0, columnspan=3, sticky="nw", padx=5, pady=20)
 
 
 window.mainloop()
