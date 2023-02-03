e322d6bd60974e1268dc2037f0e11ae1da494939
alexserante <alexandre.serante@gmail.com>
2023-02-03 10:34:09 -0300

    Update field_analysis.py

--- field_analysis/field_analysis.py
+++ field_analysis/field_analysis.py
@@ -1,12 +1,12 @@
 from pylinac import FieldAnalysis, Protocol, Centering, Edge, Normalization, Interpolation
 
-my_file = r"C:/Users/alexandre.serante/Desktop/flood_field/AL6_6MV_21x16.dcm"
+my_file = r"C:\Users\alexandre.serante\Desktop\github\EPID_images\flood_field\AL5_6MV_20x20_10MU.dcm"
 my_img = FieldAnalysis(path=my_file)
 
 
-my_img.analyze(protocol=Protocol.ELEKTA, centering=Centering.MANUAL, vert_position=0.5, horiz_position=0.5, 
-               interpolation=Interpolation.LINEAR, normalization_method=Normalization.MAX,
-               interpolation_resolution_mm=0.1, edge_detection_method=Edge.FWHM, invert=True)
+my_img.analyze(protocol=Protocol.VARIAN, interpolation=Interpolation.LINEAR, normalization_method=Normalization.MAX,
+               interpolation_resolution_mm=0.5, edge_detection_method=Edge.FWHM, invert=True,
+               in_field_ratio=0.8)
 
 print(my_img.results())  # print results as a string
 my_img.plot_analyzed_image()  # matplotlib image
