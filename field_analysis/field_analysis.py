858f109249ff7fb2fd30442803083a355ff59593
alexserante <alexandre.serante@gmail.com>
2023-02-09 19:03:02 -0300

    Update field_analysis.py

--- field_analysis/field_analysis.py
+++ field_analysis/field_analysis.py
@@ -1,14 +1,15 @@
 from pylinac import FieldAnalysis, Protocol, Centering, Edge, Normalization, Interpolation
 
-my_file = r"C:\Users\alexandre.serante\Desktop\github\EPID_images\flood_field\AL5_15MV_20x20_5MU.dcm"
+my_file = r"C:\Users\alexandre.serante\Desktop\github\EPID_images\flood_field\AL5_6MV_20x20_10MU.dcm"
 
-#my_img = FieldAnalysis(path=my_file) # no filter apllied to the image
-my_img = FieldAnalysis(path=my_file, filter=50) # filter applied
+#  my_img = FieldAnalysis(path=my_file) # no filter apllied to the image
+my_img = FieldAnalysis(path=my_file, filter=10)  # filter applied
 
 
-my_img.analyze(protocol=Protocol.VARIAN, interpolation=Interpolation.LINEAR, normalization_method=Normalization.MAX,
-               interpolation_resolution_mm=0.5, edge_detection_method=Edge.FWHM, invert=True,
-               in_field_ratio=0.8)
+my_img.analyze(protocol=Protocol.VARIAN, in_field_ratio=0.8, interpolation=Interpolation.LINEAR,
+               normalization_method=Normalization.MAX, centering=Centering.BEAM_CENTER,
+               interpolation_resolution_mm=0.5, edge_detection_method=Edge.INFLECTION_HILL(hill_window_ratio=0.2),
+               invert=True)
 
 print(my_img.results())  # print results as a string
 my_img.plot_analyzed_image()  # matplotlib image
