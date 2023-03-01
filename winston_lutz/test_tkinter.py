df62d8d62876802ea1bdfa7cf9f7c0d0c066f97e
alexserante <alexandre.serante@gmail.com>
2023-03-01 20:33:43 -0300

    Update test_tkinter.py

--- winston_lutz/test_tkinter.py
+++ winston_lutz/test_tkinter.py
@@ -2,23 +2,23 @@ from tkinter import *
 
 
 def sel():
-    selection = "You selected the option " + str(var.get())
-    label.config(text=selection)
+    print("You selected the option " + str(var.get()))
+
 
 
 root = Tk()
-var = IntVar()
-R1 = Radiobutton(root, text="Option 1", variable=var, value=1,
-                 command=sel)
+var = StringVar()
+R1 = Radiobutton(root, text="Option 1", variable=var, value=1)
 R1.pack(anchor=W)
 
-R2 = Radiobutton(root, text="Option 2", variable=var, value=2,
-                 command=sel)
+R2 = Radiobutton(root, text="Option 2", variable=var, value=2)
 R2.pack(anchor=W)
 
-R3 = Radiobutton(root, text="Option 3", variable=var, value=3,
-                 command=sel)
+R3 = Radiobutton(root, text="Option 3", variable=var, value=3)
 R3.pack(anchor=W)
+
+btn = Button(root, text="click here", command=sel)
+btn.pack(anchor=W)
 
 label = Label(root)
 label.pack()
