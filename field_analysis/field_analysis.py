6472c38cc1ed84e94dc4808947b983f707d1f41a
alexserante <79325755+alexserante@users.noreply.github.com>
2024-07-29 12:54:29 -0300

    s

--- field_analysis/field_analysis.py
+++ field_analysis/field_analysis.py
@@ -1,10 +1,10 @@
 from pylinac import FieldAnalysis, Protocol, Centering, Edge, Normalization, Interpolation
 
-my_file = r"C:\Users\alexandre.serante\Desktop\github\EPID_images\flood_field\20240223-AL6_6MV_20x20_5MU.dcm"
+my_file = r"C:\Users\a.serante\Desktop\temp\6FFF_23x23_5MU.dcm"
 #  my_img = FieldAnalysis(path=my_file) # no filter apllied to the image
 my_img = FieldAnalysis(path=my_file, filter=5)
 
-my_img.analyze(
+'''my_img.analyze(
     protocol=Protocol.VARIAN,
     in_field_ratio=0.8,
     interpolation=Interpolation.LINEAR,
@@ -13,9 +13,9 @@ my_img.analyze(
     centering=Centering.BEAM_CENTER,
     edge_detection_method=Edge.FWHM,
     invert=True)
-
+'''
 # FFF beams 
-'''my_img.analyze(
+my_img.analyze(
     protocol=Protocol.ELEKTA, 
     is_FFF=True, 
     in_field_ratio=0.8, 
@@ -25,7 +25,7 @@ my_img.analyze(
     edge_detection_method=Edge.INFLECTION_DERIVATIVE,  
     interpolation_resolution_mm=1, 
     invert=True)
-'''
+
 
 
 print(my_img.results())  # print results as a string
